e5c1f5dccaa4b8a7d002eb71e837a32f9e3bb9a5
ahdeyyy <adeala.adegb@gmail.com>
2023-08-15 19:51:32 +0100

    feat: send and recieve messages

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html/v2"
-	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type Message struct {
+	Id      string
+	Content string
+	Sender  string
+	Created time.Time
+}
+
 func main() {
+	connStr := "postgres://ade:password@127.0.0.1:5432/chat?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
@@ -37,18 +49,63 @@ func main() {
 		return c.Render("index", fiber.Map{"User": username})
 	})
 
-	app.Post("/new_message", func(c *fiber.Ctx) error {
-		log.Println(c.FormValue("text"))
-		return c.Render("messages", fiber.Map{})
+	app.Get("/messages", func(c *fiber.Ctx) error {
+		messages, e := getMessages(db)
+		if e != nil {
+			log.Println(e) // TODO: render an error dialog
+		}
+
+		return c.Render("messages", fiber.Map{"messages": messages})
+
 	})
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("error, could not load envieonment variable")
-	}
-	db_url := os.Getenv("DB_URL")
-	fmt.Println(db_url)
+	app.Post("/new_message", func(c *fiber.Ctx) error {
+		log.Println(c.FormValue("text"))
+		content := c.FormValue("text")
+		sender := c.Cookies("username")
+		stmt := `INSERT into message("content","sender")
+		VALUES($1,$2)
+		`
+		_, err = db.Exec(stmt, content, sender)
+		if err != nil {
+			log.Println(err)
+		}
+		messages, err := getMessages(db)
+		if err != nil {
+			log.Println(err)
+		}
+		return c.Render("messages", fiber.Map{"messages": messages})
+	})
+
 	app.Listen(":3000")
+}
+
+func getMessages(db *sql.DB) ([]Message, error) {
+
+	stmt := `SELECT * FROM message`
+	messages := make([]Message, 0)
+	resultRows, e := db.Query(stmt)
+	if e != nil {
+		return nil, e
+
+	}
+	defer resultRows.Close()
+
+	for resultRows.Next() {
+		msg := Message{}
+		e = resultRows.Scan(&msg.Id, &msg.Content, &msg.Sender, &msg.Created)
+		if e != nil {
+			log.Println(e) //TODO: Render an error dialog
+			return nil, e
+		}
+		messages = append(messages, msg)
+	}
+
+	if err := resultRows.Err(); err != nil {
+		log.Println(err)
+		return nil, e
+	}
+	return messages, nil
 }
 
 func generateUsername() string {
